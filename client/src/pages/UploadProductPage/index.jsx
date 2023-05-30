1088a3936384aa27f89dc50650a57f1ceb34ff9f
reader-wh94 <sstt9884@gmail.com>
2023-05-30 19:07:50 +0900

    [feat] Upload image files

--- client/src/pages/UploadProductPage/index.jsx
+++ client/src/pages/UploadProductPage/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import axiosInstance from '../../utils/axios';
 import { useNavigate } from 'react-router-dom';
+import FileUpload from './../../components/FileUpload';
 
 const continents = [
   {key:1, value: 'Africa'},
@@ -34,6 +35,13 @@ const UploadProductPage = () => {
     }));
   };
 
+  const handleImages = (newImages) => {
+    setProduct((prevState) => ({
+      ...prevState,
+      images: newImages
+    }));
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -57,6 +65,7 @@ const UploadProductPage = () => {
       </div>
 
       <form className='mt-6' onSubmit={handleSubmit}>
+        <FileUpload images={product.images} onImageChange={handleImages}/>
         <div className='mt-4'>
           <label htmlFor='title'>이름</label>
           <input 
