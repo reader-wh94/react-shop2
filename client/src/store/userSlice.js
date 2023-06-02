e9e09451465df4f92b0629ff342f10fb72ba3106
reader-wh94 <sstt9884@gmail.com>
2023-06-02 18:25:01 +0900

    [feat] Add cart page functions (add, delete)

--- client/src/store/userSlice.js
+++ client/src/store/userSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { registerUser, loginUser, authUser, logoutUser, addToCart } from "./thunkFunctions";
+import { registerUser, loginUser, authUser, logoutUser, addToCart, getCartItems, removeCartItem } from "./thunkFunctions";
 import { toast } from "react-toastify";
 
 const initialState = {
@@ -89,8 +89,35 @@ const userSlice = createSlice({
         state.isLoading = false;
         state.error = action.payload;
         toast.error(action.payload);
-      });
+      })
 
+      .addCase(getCartItems.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.cartDetail = action.payload;
+      })
+      .addCase(getCartItems.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+        toast.error(action.payload);
+      })
+
+      .addCase(removeCartItem.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(removeCartItem.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.cartDetail = action.payload.productInfo;
+        state.userData.cart = action.payload.cart;
+        toast.info('상품이 장바구니에서 삭제되었습니다.');
+      })
+      .addCase(removeCartItem.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+        toast.error(action.payload);
+      });
   }
 });
 
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Product = require('../models/Product');
 const User = require('../models/User');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
@@ -94,6 +95,37 @@ router.post('/cart', auth, async (req, res, next) => {
       )
       return res.status(201).send(user.cart);
     }
+  } catch (error) {
+      next(error);
+  }
+});
+
+router.delete('/cart', auth, async (req, res, next) => {
+  try {
+      // 먼저 cart안에 지우려고 한 상품을 지워주기
+      const userInfo = await User.findOneAndUpdate(
+          { _id: req.user._id },
+          {
+              "$pull":
+                  { "cart": { "id": req.query.productId } }
+          },
+          { new: true }
+      );
+
+      const cart = userInfo.cart;
+      const array = cart.map(item => {
+          return item.id;
+      });
+
+      const productInfo = await Product
+          .find({ _id: { $in: array } })
+          .populate('writer')
+
+      return res.json({
+          productInfo,
+          cart
+      });
+
   } catch (error) {
       next(error);
   }
